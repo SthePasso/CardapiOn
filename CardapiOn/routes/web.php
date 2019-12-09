42c05a8d5f1c4ea4bf1dfd5a9f7d85a316897563
SthePasso <sjgp@icomp.ufam.edu.br>
2019-12-09 18:58:55 -0400

    Front End em Andamento

---
+++ CardapiOn/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts/home');
+});
+
+Route::get('register', function () {
+    return view('auth/register');
+});
+
+Route::get('login', function () {
+    return view('auth/login');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
